4cbaf52fa21b940df9d04046a6b50df893a9cf3d
djk239 <dale.king@rutgers.edu>
2023-12-28 21:34:09 -0500

    worked on media quieres, hero section works good on mobile as does the nav, nav needs working collasp menu added

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,18 +1,21 @@
+import React from 'react';
 import styles from "./Navbar.module.css"
 import image from '../../data/icons_assets/Logo.svg'
+import menuicon from '../../data/icons_assets/icon _hamburger menu_.svg'
 
 function Navbar (){
     return (
         <nav className={styles.navbar}>
             <img className={styles.image} src={image} alt="compnay logo display here" />
             <ul className={styles.listcontainer}>
-                <li>Home</li>
-                <li>About</li>
-                <li>Menu</li>
-                <li>Reservations</li>
-                <li>Order Online</li>
-                <li>Login</li>
+                <a href="" >Home</a>
+                <a href="" >About</a>
+                <a href="" >Menu</a>
+                <a href="" >Reservations</a>
+                <a href="" >Order Online</a>
+                <a href="" >Login</a>
             </ul>
+            <img className={styles.menu} src={menuicon} alt="hamburger menu icon"/>
         </nav>
     )
 }
